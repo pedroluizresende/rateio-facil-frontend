ffb56c4803a82dd5caf19d1a0eb96dffe411f58d
Pedro Resende <pedroluizrnunez@gmail.com>
2023-11-10 15:59:18 -0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,11 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rateio-facil-frontend/',
   define: {
     'process.env': {
       REACT_APP_API_URL: JSON.stringify(process.env.REACT_APP_API_URL),
       SECRET_KEY: JSON.stringify(process.env.SECRET_KEY),
     },
-  },
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
-    port: 5173,
   },
 });
