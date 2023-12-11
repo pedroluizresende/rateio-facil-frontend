a4b600c709787c19a32b046e803bff86d190f400
Pedro Resende <pedroluizrnunez@gmail.com>
2023-12-11 12:27:10 -0300

    fix: retira logo da vite como fav icon

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,5 +1,6 @@
 const ANATOURIZED = 403;
 const NOT_FOUND = 404;
+const INTERNAL_SERVER_ERROR = 500;
 
 const handleApiError = (error, setError, navigate) => {
   if (error.response) {
@@ -10,6 +11,12 @@ const handleApiError = (error, setError, navigate) => {
     }
     if (error.response.status === NOT_FOUND) {
       navigate('/404');
+    }
+
+    if (error.response.status === INTERNAL_SERVER_ERROR) {
+      setError('Ocorreu um erro inesperado ao se comunicar com o servidor.');
+      localStorage.clear();
+      navigate('/');
     }
   } else {
     setError('Ocorreu um erro inesperado ao se comunicar com o servidor.');
