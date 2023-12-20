fb012d724b289953f975b3734043ae286a541d9b
Pedro Resende <pedroluizrnunez@gmail.com>
2023-12-20 10:22:13 -0300

    feat-finaliza funçao de tirar foto do momento

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -12,8 +12,8 @@ function Camera({ setOpenCamera, setCurrentImage }) {
   const getVideo = () => {
     navigator.mediaDevices.getUserMedia({
       video: {
-        width: 3840,
-        height: 2160,
+        width: 2160,
+        height: 3840,
         facingMode: switchCamera ? 'user' : 'environment',
       },
 
