12c898b4c15e4406227f487f1a52d2d4a1e0ed08
Pedro Resende <pedroluizrnunez@gmail.com>
2023-12-20 11:12:12 -0300

    altera calccard adicionando toFixed na taxa de serviço

--- src/components/CalcCard.jsx
+++ src/components/CalcCard.jsx
@@ -29,7 +29,7 @@ function CalcCard({ calculation }) {
               Taxa de Serviço:
             </strong>
             <p>
-              {`R$ ${taxService}`}
+              {`R$ ${taxService.toFixed(2)}`}
             </p>
           </li>
           <li>
